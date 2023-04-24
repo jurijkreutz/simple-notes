8c0496277f28636d1d0d9092f991956add8ae06d
jurijkreutz <jurij.kreutz97@gmail.com>
2023-04-24 16:52:49 +0200

    chore: set up project

---
+++ backend/src/main/java/com/codecool/simplenotes/SimplenotesApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.simplenotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplenotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplenotesApplication.class, args);
+	}
+
+}
