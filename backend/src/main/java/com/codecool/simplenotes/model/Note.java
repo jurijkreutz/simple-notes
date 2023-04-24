e5a8f9744554297c8b86cb2df22d36a6ee2be158
jurijkreutz <jurij.kreutz97@gmail.com>
2023-04-24 16:57:40 +0200

    feat: create note class

---
+++ backend/src/main/java/com/codecool/simplenotes/model/Note.java
@@ -0,0 +1,16 @@
+package com.codecool.simplenotes.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Note {
+
+    private String title;
+
+    private String content;
+
+}
