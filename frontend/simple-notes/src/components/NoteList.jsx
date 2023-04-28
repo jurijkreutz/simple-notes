a3525153bc2369620f378b788a2a163fcdbfad73
jurijkreutz <jurij.kreutz97@gmail.com>
2023-04-28 12:40:36 +0200

    feat: implement flexbox design for notes

--- frontend/simple-notes/src/components/NoteList.jsx
+++ frontend/simple-notes/src/components/NoteList.jsx
@@ -5,7 +5,7 @@ import Note from './Note'
 export default function NoteList({ notes, updateNotes }) {
 
   return (
-    <div>
+    <div id="noteList">
       {notes.map((note) => (
         <Note note={note} key={note.id} updateNotes={updateNotes} />
       ))}
